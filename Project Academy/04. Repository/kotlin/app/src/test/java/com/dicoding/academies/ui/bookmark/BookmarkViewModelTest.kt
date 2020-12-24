a7b118d15a69fd1cfd3d28010ee2d7e5eb99049d
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-12-24 19:13:42 +0700

    vm test

--- Project Academy/04. Repository/kotlin/app/src/test/java/com/dicoding/academies/ui/bookmark/BookmarkViewModelTest.kt
+++ Project Academy/04. Repository/kotlin/app/src/test/java/com/dicoding/academies/ui/bookmark/BookmarkViewModelTest.kt
@@ -1,22 +1,35 @@
 package com.dicoding.academies.ui.bookmark
 
+import com.dicoding.academies.data.AcademyRepository
+import com.dicoding.academies.utils.DataDummy
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
 
-
+@RunWith(MockitoJUnitRunner::class)
 class BookmarkViewModelTest {
     private lateinit var viewModel: BookmarkViewModel
 
+    @Mock
+    private lateinit var academyRepository: AcademyRepository
+
     @Before
     fun setUp() {
-        viewModel = BookmarkViewModel()
+        viewModel = BookmarkViewModel(academyRepository)
     }
 
     @Test
     fun getBookmark() {
+        `when`(academyRepository.getBookmarkedCourses()).thenReturn(DataDummy.generateDummyCourses())
         val courseEntities = viewModel.getBookmarks()
+
+        verify(academyRepository).getBookmarkedCourses()
         assertNotNull(courseEntities)
         assertEquals(5, courseEntities.size)
     }
