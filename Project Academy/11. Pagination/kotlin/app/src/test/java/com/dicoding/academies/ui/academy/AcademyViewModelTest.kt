8bf97bde435b5351ac07ad52db66dd7cb4fbb085
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2021-01-05 13:27:04 +0700

    errr

--- Project Academy/11. Pagination/kotlin/app/src/test/java/com/dicoding/academies/ui/academy/AcademyViewModelTest.kt
+++ Project Academy/11. Pagination/kotlin/app/src/test/java/com/dicoding/academies/ui/academy/AcademyViewModelTest.kt
@@ -3,9 +3,9 @@ package com.dicoding.academies.ui.academy
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.paging.PagedList
 import com.dicoding.academies.data.AcademyRepository
 import com.dicoding.academies.data.source.local.entity.CourseEntity
-import com.dicoding.academies.utils.DataDummy
 import com.dicoding.academies.vo.Resource
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
@@ -30,7 +30,10 @@ class AcademyViewModelTest {
     private lateinit var academyRepository: AcademyRepository
 
     @Mock
-    private lateinit var observer: Observer<Resource<List<CourseEntity>>>
+    private lateinit var observer: Observer<Resource<PagedList<CourseEntity>>>
+
+    @Mock
+    private lateinit var pagedList: PagedList<CourseEntity>
 
     @Before
     fun setUp() {
@@ -39,8 +42,9 @@ class AcademyViewModelTest {
 
     @Test
     fun getCourses() {
-        val dummyCourses = Resource.success(DataDummy.generateDummyCourses())
-        val courses = MutableLiveData<Resource<List<CourseEntity>>>()
+        val dummyCourses = Resource.success(pagedList)
+        `when`(dummyCourses.data?.size).thenReturn(5)
+        val courses = MutableLiveData<Resource<PagedList<CourseEntity>>>()
         courses.value = dummyCourses
 
         `when`(academyRepository.getAllCourses()).thenReturn(courses)
